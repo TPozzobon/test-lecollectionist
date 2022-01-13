6e8d530d5921b540e3da97ed92bacdbd4b7a3943
Thomas Pozzobon <thomaspozzobon86@gmail.com>
2022-01-13 14:59:22 +0100

    build: add method create to schedules controller and add schedules in shops index view

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -1,2 +1,23 @@
+# frozen_string_literal: true
+
 class SchedulesController < ApplicationController
+  def create
+    @shop = Shop.find(params[:shop_id])
+
+    @schedule = Schedule.new(schedule_params)
+
+    @schedule.shop = @shop
+
+    if @schedule.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def schedule_params
+    params.require(:schedule).permit(:day)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -10,8 +10,17 @@
 
 puts 'Cleaning database'
 Shop.destroy_all
+Schedule.destroy_all
 
 puts 'Creating shops'
 Shop.create!(name: 'Soyez Semi-Croustillant')
-Shop.create!(name: 'Au chevalier au pancreas')
+
+puts 'Creating schedules'
+Schedule.create!(day: 'Lundi', open_am: '10:30', close_am: '15:00', open_pm: '17:00', close_pm: '20:00', shop_id: 1)
+Schedule.create!(day: 'Mardi', open_am: '10:30', close_am: '15:00', open_pm: '17:00', close_pm: '20:00', shop_id: 1)
+Schedule.create!(day: 'Mercredi', open_am: '10:30', close_am: '15:00', open_pm: '17:00', close_pm: '20:00', shop_id: 1)
+Schedule.create!(day: 'Jeudi', open_am: '10:30', close_am: '15:00', open_pm: '17:00', close_pm: '20:00', shop_id: 1)
+Schedule.create!(day: 'Vendredi', open_am: '10:30', close_am: '15:00', open_pm: '17:00', close_pm: '20:00', shop_id: 1)
+Schedule.create!(day: 'Samedi', open_am: '10:30', close_pm: '20:00', shop_id: 1)
+Schedule.create!(day: 'Dimanche', shop_id: 1)
 puts 'Finished !'
