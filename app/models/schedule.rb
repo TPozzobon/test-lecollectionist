a9aa0a6bc7d5c5c3191c7a7eaf998e84a9cac1aa
Thomas Pozzobon <thomaspozzobon86@gmail.com>
2022-01-18 17:39:55 +0100

    test: add model test for shop and schedule

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,3 +1,12 @@
+# frozen_string_literal: true
+
 class Schedule < ApplicationRecord
   belongs_to :shop
+
+  validates :day, presence: true
+  validate :consistency_schedules
+
+  def consistency_schedules
+    errors.add('please add a correct hour') unless :open_am > :close_am || :close_am > :open_pm || :open_pm > :close_pm
+  end
 end
--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,3 +1,7 @@
+# frozen_string_literal: true
+
 class Shop < ApplicationRecord
   has_many :schedules
+
+  validates :name, presence: true
 end
